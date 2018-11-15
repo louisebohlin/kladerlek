a30c852f91ab3cd1978042cb113483be90730f54
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2018-11-15 13:31:43 +0100

    Styled weather fonts

--- frontend/src/pages/startpage/weather.js
+++ frontend/src/pages/startpage/weather.js
@@ -6,9 +6,9 @@ class Weather extends React.Component {
   render() {
     return (
       <div className="weatherWrapper">
-        <h2>{this.props.city}</h2>
-        <h2>{this.props.temperature}</h2>
         <h2>{this.props.description}</h2>
+        <p>{this.props.city}</p>
+        <h1>{this.props.temperature}</h1>
       </div>
 
     )
