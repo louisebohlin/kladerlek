d48e137e389f5148594a82f77ce54a1440033ffe
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2018-11-15 11:11:19 +0100

    Fetching works and shows

--- frontend/src/pages/startpage/startpage.js
+++ frontend/src/pages/startpage/startpage.js
@@ -3,28 +3,38 @@ import { HashRouter as Router, Route, Link } from "react-router-dom"
 import Header from  "../../components/header/header.js"
 import Product from "./product.js"
 import Button from "../../components/button/button.js"
+import Weather from "./weather.js"
 import "./startpage.scss"
 
 const productsJson = require("../../products.json")
 
 class StartPage extends React.Component {
   state = {
-    city: "Stockholm,Sweden",
+    city: "Stockholm, Sweden",
     filter: "",
     weather: null
   }
 
-  componentDidMount() {
-    this.getWeather()
-  }
+componentDidMount() {
+  this.getWeather()
+}
 
-  getWeather = () => {
+handleCityChange = event => {
+  this.setState({
+    city: event.target.value,
+  })
+}
+
+  getWeather = (e) => {
     const weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&units=metric&APPID=ae7f18bdb699e35f5dd3399dba9247c1`;
     fetch(weatherAPI)
       .then(response => response.json())
       .then(json => {
         this.setState({
-          weather: json
+          weather: json,
+          city: json.name,
+          temperature: json.main.temp.toFixed(1),
+          description: json.weather[0].description
         })
       }).catch(err => {
         console.log(err)
@@ -33,26 +43,19 @@ class StartPage extends React.Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.city !== this.state.city) {
-      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&units=metric&APPID=ae7f18bdb699e35f5dd3399dba9247c1`)
-        .then(response => response.json())
-        .then(json => {
-          console.log(json)
-          this.setState({
-            weather: json })
-        })
+      this.getWeather()
     }
   }
 
-  handleCityChange = event => {
-    this.setState({
-      city: event.target.value
-    })
-  }
+
+
+
   render() {
     return (
       <div>
       <div className="startPageWrapper">
       <Header />
+
 
       <div className="cityDropdownWrapper">
 
@@ -66,6 +69,11 @@ class StartPage extends React.Component {
         </select>
       </div>
       </div>
+
+
+      <Weather city={this.state.city}
+               temperature={this.state.temperature}
+               description={this.state.description} />
 
 
       <div className="heroImageWrapper">
---
+++ frontend/src/pages/startpage/weather.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+class Weather extends React.Component {
+
+  render() {
+    return (
+      <div className="weatherWrapper">
+        <h2>{this.props.city}</h2>
+        <h2>{this.props.temperature}</h2>
+        <h2>{this.props.description}</h2>
+      </div>
+
+    )
+  }
+
+}
+
+export default Weather
